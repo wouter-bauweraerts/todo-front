dec527f4d46e8be426dea80a7d82bb36a7eb4c72
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2023-02-06 16:48:43 +0100

    optimize import

--- src/app/statemanagement/reducers/core.reducer.ts
+++ src/app/statemanagement/reducers/core.reducer.ts
@@ -1,7 +1,6 @@
 import {CoreState} from '../app-state';
 import {createReducer, on} from '@ngrx/store';
 import * as CoreActions from '../actions/core.actions';
-import {ErrorType} from '../../types/core/error.type';
 
 export const initialState: CoreState = {
   error: undefined,
